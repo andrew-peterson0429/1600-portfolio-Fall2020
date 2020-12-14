8318336161e8a81b3a245b69c4c67612545076d9
Andrew Peterson <andrewpeterson1021@gmail.com>
2020-12-14 00:07:56 -0700

    updated code

--- pokemon/demo.js
+++ pokemon/demo.js
@@ -1,4 +1,5 @@
-const typesAPI = 'https://pokeapi.co/api/v2/type/'
+const typesAPI = 'https://pokeapi.co/api/v2/type/';
+const pokemonGrid = document.querySelector('.pokemonGrid');
 
 // Inits on page load. Populates the type and limit picklists.
 const initOptions = function (url, max, min) {
@@ -29,16 +30,24 @@ const initOptions = function (url, max, min) {
   // Populate event listener on button.
   let button = document.getElementById('popCards');
   button.addEventListener('click', () => {
-    popCards();
+    popCards(getInput());
   });
 
 }(typesAPI, 50, 10);
+
+// Get input from the DOM.
+function getInput() {
+  let input = {
+    type: document.getElementById('type').value,
+    limit: document.getElementById('limit').value
+  }
+  return input;
+};
 
 // Gets a list of pokemon based on the users selection 
 const getPokemon = function (url, type, quantity) {
   let results = [];
   let selectedType;
-  let limit = quantity;
 
   // Get json data of pokemon types.
   axios.get(url).then((res) =>{
@@ -50,9 +59,22 @@ const getPokemon = function (url, type, quantity) {
         let iteration = outerList[i];
         axios.get(iteration.url).then((res) => {
           let innerList = res.data.pokemon;
-          if (!limit) limit = innerList.length;
-          for (let ii = 0; ii < limit; ii++) {
-            results.push(innerList[ii]);
+          for (let ii = 0; ii < innerList.length; ii++) {
+            if (ii < quantity) {
+              axios.get(innerList[ii].pokemon.url).then((res) => {
+                if (res.data.sprites.front_default) {
+                  let pokemon = {
+                    img: res.data.sprites.front_default, 
+                    name: res.data.name,
+                    weight: res.data.weight, 
+                    height: res.data.height
+                  };
+                  results.push(pokemon);
+                };
+              }).catch((err) => {
+                console.log(err);
+              });
+            };
           };
         }).catch((err) => {
           console.log(err);
@@ -64,9 +86,22 @@ const getPokemon = function (url, type, quantity) {
         selectedType = outerList[i]; 
         axios.get(selectedType.url).then((res) => {
           let innerList = res.data.pokemon;
-          if (!limit) limit = innerList.length;
-          for (let ii = 0; ii < limit; ii++) {
-            results.push(innerList[ii]);
+          for (let ii = 0; ii < innerList.length; ii++) {
+            if (ii < quantity) {
+              axios.get(innerList[ii].pokemon.url).then((res) => {
+                if (res.data.sprites.front_default) {
+                  let pokemon = {
+                    img: res.data.sprites.front_default, 
+                    name: res.data.name,
+                    weight: res.data.weight, 
+                    height: res.data.height
+                  };
+                  results.push(pokemon);
+                };
+              }).catch((err) => {
+                console.log(err);
+              });
+            };
           };
         }).catch((err) => {
           console.log(err);
@@ -76,80 +111,65 @@ const getPokemon = function (url, type, quantity) {
   }).catch((err) => {
     console.log(err);
   });
-  console.log(results);
   return results;
 }; 
 
-const popCards = function () {
-  let userSelectedType = document.getElementById('type').value;
-  let userSelectedLimit = document.getElementById('limit').value;
-  let pokemon = null;
-  let pokemonGrid = document.querySelector('pokemonGrid');
-  // while (pokemonGrid.firstChild) {
-  //   pokemonGrid.removeChild(pokemonGrid.firstChild);
-  // }
-  pokemon = getPokemon(typesAPI, userSelectedType, userSelectedLimit);
+const popCards = async (input) => {
 
-  let pokeScene = document.createElement('div');
-  pokeScene.className = 'scene';
-  let pokeCard = document.createElement('div');
-  pokeCard.className = 'card';
-  pokeCard.addEventListener('click', () => {
-      pokeCard.classList.toggle('is-flipped');
-  });
+  const deck = await getPokemon(typesAPI, input.type, input.limit);
+
+  setTimeout(function() { buildDeck(deck) }, 1000);
   
-  pokeCard.appendChild(populateCardFront(pokemon))
-  pokeCard.appendChild(populateCardBack(pokemon))
-  pokeScene.appendChild(pokeCard)
-  pokemonGrid.appendChild(pokeScene)
-};
+  // Clear the grid of all previous content.
+  const clearGrid = function() {
+    while (pokemonGrid.firstChild) {
+      pokemonGrid.removeChild(pokemonGrid.lastChild);
+    };
+    console.log('Grid cleared!');
+  }();
 
-function populateCardFront(pokemon) {
+  // Populate the front of the card.
+  function popCardFront(pokemon) {
+    let cardFront = document.createElement('div');
+    cardFront.className = `card__face card__face--front`;
+    let frontLabel = document.createElement('p');
+    let frontImage = document.createElement('img');
+    frontLabel.textContent = pokemon.name;
+    frontImage.src = pokemon.img;
+    cardFront.appendChild(frontImage);
+    cardFront.appendChild(frontLabel);
+    return cardFront;
+  };
 
-  let cardFront = document.createElement('div');
-  cardFront.className = `card__face card__face--front`;
-  let frontLabel = document.createElement('p');
-  let frontImage = document.createElement('img');
-  frontLabel.textContent = pokemon.name;
-  frontImage.src = getImageFileName(pokemon)
-  cardFront.appendChild(frontImage)
-  cardFront.appendChild(frontLabel)
-  return cardFront
-};
+  // Popuate the back of the card.
+  function popCardBack(pokemon) {
+    let cardBack = document.createElement('div');
+    cardBack.className = 'card__face card__face--back';
+    let heightLabel = document.createElement('h3');
+    let weightLabel = document.createElement('h3');
+    heightLabel.textContent = pokemon.height;
+    weightLabel.textContent = pokemon.weight;
+    cardBack.appendChild(heightLabel);
+    cardBack.appendChild(weightLabel);
+    return cardBack;
+  };
 
-function populateCardBack(pokemon) {
-  let cardBack = document.createElement('div')
-  cardBack.className = 'card__face card__face--back'
-  let backLabel = document.createElement('h3')
-  backLabel.textContent = `Abilities:`
-  let abilityList = document.createElement('ul')
-  pokemon.abilities.forEach(ability => {
-      let abilityName = document.createElement('li')
-      abilityName.textContent = ability.ability.name
-      abilityList.appendChild(abilityName)
-  });
-  let movesLabel = document.createElement('h3')
-  movesLabel.textContent = 'Most Accurate Move:'
-  let moveAccuracy = document.createElement('h4')
-  const mostAccurateMove = getBestAccuracyAndPower(pokemon.moves)
-  cardBack.appendChild(backLabel)
-  cardBack.appendChild(abilityList)
-  cardBack.appendChild(movesLabel)
-  cardBack.appendChild(moveAccuracy)
-  return cardBack
-};
+  const buildDeck = async (array) => {
+    let list = await array;
+    console.log(list)
+    for (let i = 0; i < list.length; i++) {
+      let pokeScene = document.createElement('div');
+      pokeScene.className = 'scene';
+      let pokeCard = document.createElement('div');
+      pokeCard.className = 'card';
+      pokeCard.addEventListener('click', () => {
+        pokeCard.classList.toggle('is-flipped')
+      });
 
-// function getBestAccuracyAndPower(pokemoves) {
-//   return pokemoves.reduce((mostAccurate, move) => {
-//       getAPIData(move.move.url).then
-//           (async (data) => {
-//           })
-//     }, {});
-// };
-
-function getPokeData(url) {
-  axios.get(url).then((res) => {
-    let pokemon = res.data;
-    return pokemon;
-  });
+      pokeCard.appendChild(popCardFront(list[i]));
+      pokeCard.appendChild(popCardBack(list[i]));
+      pokeScene.appendChild(pokeCard);
+      pokemonGrid.appendChild(pokeScene);
+    };
+  };
 };
